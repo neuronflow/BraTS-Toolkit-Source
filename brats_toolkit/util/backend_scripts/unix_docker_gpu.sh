e697f9513fc99d64b356cb8cf6862d500e02cfba
Florian Kofler <florian.sebastian.kofler@gmail.com>
2020-02-14 11:32:34 +0100

    roll back users for the moment

--- brats_toolkit/util/backend_scripts/unix_docker_gpu.sh
+++ brats_toolkit/util/backend_scripts/unix_docker_gpu.sh
@@ -4,8 +4,9 @@ if [ $# -eq 0 ]; then
   echo "You must enter the number of desired workers and 5 paths!, e.g. docker_run.sh 2 /setting /dicom_import /nifti_export /exam_import /exam_export"
   exit 1
 fi
+
 docker stop greedy_elephant
-docker run --rm -d --name=greedy_elephant --user $(id -u):$(id -g) --gpus all -p 5000:5000 -p 9181:9181 -v "$2":"/data/import/dicom_import" -v "$3":"/data/export/nifti_export" -v "$4":"/data/import/exam_import" -v "$5":"/data/export/exam_export" projectelephant/server redis-server
+docker run --rm -d --name=greedy_elephant --user userid:usergroup --gpus all -p 5000:5000 -p 9181:9181 -v "$2":"/data/import/dicom_import" -v "$3":"/data/export/nifti_export" -v "$4":"/data/import/exam_import" -v "$5":"/data/export/exam_export" projectelephant/server redis-server
 #wait until everything is started up
 sleep 10
 #start x-server for non-gui gui
@@ -14,3 +15,9 @@ docker exec -d greedy_elephant python3 elephant_server.py
 docker exec -d greedy_elephant /bin/bash -c "source ~/.bashrc; rq-dashboard;"
 #ugly format to set correct path variable every time! (as .bashrc doesn't want to work)
 docker exec -d greedy_elephant /bin/bash -c "source ~/.bashrc; ./start_workers.sh;"
+
+# TODO fix user thing, also need to add user on exec
+# userid=$(id -u)
+# usergroup=$(id -g)
+# echo $userid:$usergroup
+# --user $userid:$usergroup
